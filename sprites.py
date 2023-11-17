39c857b0dddfa8c85d30420389a4a51d1a63c8e5
EthanLien1 <142935105+EthanLien1@users.noreply.github.com>
2023-11-16 18:53:49 -0800

    Update sprites.py

---
+++ sprites.py
@@ -0,0 +1,96 @@
+# This file was created by: Ethan Lien
+import pygame as pg
+from pygame.sprite import Sprite
+
+from pygame.math import Vector2 as vec
+import os
+from settings import *
+
+# setup asset folders here - images sounds etc.
+game_folder = os.path.dirname(__file__)
+img_folder = os.path.join(game_folder, 'images')
+snd_folder = os.path.join(game_folder, 'sounds')
+
+class Player(Sprite):
+    def __init__(self, game):
+        Sprite.__init__(self)
+        # self.image = pg.Surface((50, 50))
+        # self.image.fill(GREEN)
+        # use an image for player sprite...
+        self.game = game
+        self.image = pg.image.load(os.path.join(img_folder, 'theBigBell.png')).convert()
+        self.image.set_colorkey(BLACK)
+        self.rect = self.image.get_rect()
+        self.rect.center = (0, 0)
+        self.pos = vec(WIDTH/2, HEIGHT/2)
+        self.vel = vec(0,0)
+        self.acc = vec(0,0) 
+    # defines what controls do what
+    def controls(self):
+        keys = pg.key.get_pressed()
+        if keys[pg.K_a]:
+            self.acc.x = -5
+        if keys[pg.K_d]:
+            self.acc.x = 5
+        if keys[pg.K_SPACE]:
+            self.jump()
+# causes the platforms to dissaper
+    def jump(self):
+        hits = pg.sprite.spritecollide(self, self.game.all_platforms, True)
+        if hits:
+            self.vel.y = -PLAYER_JUMP
+            self.game.new_platform()
+        # defines properties for player
+    def update(self):
+        self.acc = vec(0,PLAYER_GRAV)
+        self.controls()
+        # if friction - apply here
+        self.acc.x += self.vel.x * -PLAYER_FRIC
+        # self.acc.y += self.vel.y * -0.3
+        # equations of motion
+        self.vel += self.acc
+        self.pos += self.vel + 0.5 * self.acc
+        self.rect.midbottom = self.pos
+        if self.rect.top > HEIGHT:
+            self.game.lives -= 1
+
+# platforms
+# defines whether platforms are moving, color, and speed
+class Platform(Sprite):
+    def __init__(self, x, y, w, h, category):
+        Sprite.__init__(self)
+        self.image = pg.Surface((w, h))
+        self.image.fill(AQUA)
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.category = category
+        self.speed = 0
+        if self.category == "moving":
+            self.speed = 5
+    # updates the platform
+    def update(self):
+        if self.category == "moving":
+            self.rect.x += self.speed
+            if self.rect.x + self.rect.w > WIDTH or self.rect.x < 0:
+                self.speed = -self.speed
+
+# defines the properties of the class mob
+class Mob(Sprite):
+    def __init__(self, x, y, w, h, kind):
+        Sprite.__init__(self)
+        self.image = pg.Surface((w, h))
+        self.image.fill(RED)
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.kind = kind
+        self.pos = vec(WIDTH/2, HEIGHT/2)
+        self.vel = (0, 10)
+    def update(self):
+        pass
+
+
+
+
+  
