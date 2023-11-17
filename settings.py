985c359af5636b33364b59061436e1d758e5f2d3
EthanLien1 <142935105+EthanLien1@users.noreply.github.com>
2023-11-16 18:52:52 -0800

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,26 +1,38 @@
+# This file was created by: Ethan Lien
+# Content from Chris Bradfield; Kids Can Code
+# KidsCanCode - Game Development with Pygame video series
+# Video link: https://youtu.be/OmlQ0XCvIn0 
+
 # game settings 
-WIDTH = 360
-HEIGHT = 480
+WIDTH = 720
+HEIGHT = 720
 FPS = 30
-SCORE = 0
 
 # player settings
 PLAYER_JUMP = 30
 PLAYER_GRAV = 1.5
+global PLAYER_FRIC
+PLAYER_FRIC = 0.2
 
 # define colors
+# https://www.webucator.com/article/python-color-constants-module/
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
-SKYBLUE = (150, 200, 255)
+AQUA = (0, 255, 255)
+LIGHT_BLUE = (152, 245, 255)
+BROWN = (138, 54, 15)
 
-# starting platforms
-PLATFORM_LIST = [(0, HEIGHT - 40, WIDTH, 40, " "),
-                 (WIDTH / 2 - 50, HEIGHT * 3 / 4, 100, 20, " "),
+# Font size
+FONT = 22
+# spacing
+SPACING = 20
+
+# list of all platforms
+PLATFORM_LIST = [(0, HEIGHT - 40, WIDTH, 40, "normal"),
+                 (WIDTH / 2 - 50, HEIGHT * 3 / 4, 100, 20,"normal"),
                  (125, HEIGHT - 350, 100, 20, "moving"),
-                 (350, 200, 100, 20, " "),
-                 (175, 100, 50, 20, " "),
-                 (10, 350, 50, 10, " "),
-                 (280, 350, 50, 20, " ")]
+                 (222, 200, 100, 20, "moving"),
+                 (175, 100, 50, 20, "moving")]
